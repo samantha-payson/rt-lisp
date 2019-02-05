42cda7ae8c990cab4a96104c0802f515d9f3925e
Samantha Payson <samantha.payson@protonmail.com>
2019-02-04 22:27:52 -0500

    Add LGPL header to Lisp files.
    Signed-off-by: Samantha Payson <samantha.payson@protonmail.com>

--- test.lisp
+++ test.lisp
@@ -1,3 +1,18 @@
+;;;; This file is part of RT Lisp.
+;;;;
+;;;; RT Lisp is free software: you can redistribute it and/or modify it under
+;;;; the terms of the GNU Lesser General Public License as published by the Free
+;;;; Software Foundation, either version 3 of the License, or (at your option)
+;;;; any later version.
+;;;;
+;;;; RT Lisp is distributed in the hope that it will be useful, but WITHOUT ANY
+;;;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;;;; FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+;;;; more details.
+;;;;
+;;;; You should have received a copy of the GNU Lesser General Public License
+;;;; along with RT Lisp.  If not, see <https://www.gnu.org/licenses/>.
+
 (defun map-to-neg (n)
   (if (lt 0 n)
       (insert (map-to-neg (isub n 1))
