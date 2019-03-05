4ce968cd968492aef3a97090ed58f5901d54f847
Samantha Payson <samantha.payson@protonmail.com>
2019-03-05 11:59:28 -0500

    Add copyright statement.
    Also minor fix to io:write implementation.
    
    Signed-off-by: Samantha Payson <samantha.payson@protonmail.com>

--- lisp/io.lisp
+++ lisp/io.lisp
@@ -1,3 +1,18 @@
+;;;; This file is part of RT Lisp.
+;;;;
+;;;; RT Lisp is free software: you can redistribute it and/or modify it under
+;;;; the terms of the GNU Lesser General Public License as published by the Free
+;;;; Software Foundation, either version 3 of the License, or (at your option)
+;;;; any later version.
+;;;;
+;;;; RT Lisp is distributed in the hope that it will be useful, but WITHOUT ANY
+;;;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;;;; FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+;;;; more details.
+;;;;
+;;;; You should have received a copy of the GNU Lesser General Public License
+;;;; along with RT Lisp.  If not, see <https://www.gnu.org/licenses/>.
+
 (in-package io
   (std:use-package std
 
@@ -7,9 +22,10 @@
          (let ((len (intrinsic:len io-list)))
            (rlet rec ((i 0)
                       (count 0))
-             (when (< i len)
-               (rec (+ i 1)
-                    (+ count (write f (intrinsic:get io-list i))))))))
+             (if (< i len)
+                 (rec (+ i 1)
+                      (+ count (write f (intrinsic:get io-list i))))
+               count))))
         ((cons? io-list)
          (let ((a (write f (car io-list)))
                (b (write f (cdr io-list))))
